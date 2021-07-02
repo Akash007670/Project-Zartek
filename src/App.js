fb6dacbf16040c4d9919791330f856f12427f2ad
Akash Kumar <akaysharma894@gmail.com>
2021-07-02 23:33:36 +0530

    Added Board and Card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Styles from "./App.module.css";
+import Board from "./components/Board";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={Styles["App"]}>
+      <main className={Styles["board-wrapper"]}>
+        <Board id="board-1" className={Styles["board"]}>
+          <Card id="card-1" className={Styles["card"]} draggable="true">
+            <p>Card one</p>
+          </Card>
+          <Card id="card-5" className={Styles["card"]} draggable="true">
+            <p>Card five</p>
+          </Card>
+        </Board>
+        <Board id="board-2" className={Styles["board"]}>
+          <Card id="card-2" className={Styles["card"]} draggable="true">
+            <p>Card two</p>
+          </Card>
+          <Card id="card-3" className={Styles["card"]} draggable="true">
+            <p>Card three</p>
+          </Card>
+          <Card id="card-4" className={Styles["card"]} draggable="true">
+            <p>Card four</p>
+          </Card>
+        </Board>
+      </main>
     </div>
   );
 }
